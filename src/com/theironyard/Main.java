99f4800553a08802cfac21d2c63568f06935bbf7
JackNel <jcneligan@gmail.com>
2015-10-26 14:23:34 -0400

    Add create user and get post

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,41 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<User> users = new ArrayList();
+        Spark.staticFileLocation("/public");
+        Spark.init();
+        Spark.get(
+                "/posts",
+                ((request1, response1) -> {
+                    HashMap m = new HashMap();
+                    m.put("name", users);
+                    m.put("posts", users);
+                    return new ModelAndView(m, "posts.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/create-user",
+                ((request, response) -> {
+                    User user = new User();
+                    user.name = request.queryParams("username");
+                    users.add(user);
+                    response.redirect("/posts");
+                    return("");
+
+                })
+        );
+       Spark.post(
+                "/create-post",
+        );
     }
 }
